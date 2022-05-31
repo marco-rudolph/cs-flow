ea2bb84d06c655da64453d0a71f348ca9678e0ae
marco-rudolph <rudolph@tnt.uni-hannover.de>
2022-05-31 13:46:55 +0200

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -174,7 +174,7 @@ def evaluate(model, test_loader):
                 likelihood_grouped = list()
                 for i in range(len(z)):
                     z_grouped.append(z[i].view(-1, *z[i].shape[1:]))
-                    likelihood_grouped.append(torch.mean(z_grouped[-1] ** 2, dim=(1,)) / c.n_feat)
+                    likelihood_grouped.append(torch.mean(z_grouped[-1] ** 2, dim=(1,)))
                 all_maps.extend(likelihood_grouped[0])
                 for i_l, l in enumerate(t2np(labels)):
                     # viz_maps([lg[i_l] for lg in likelihood_grouped], c.modelname + '_' + str(c.viz_sample_count), label=l, show_scales = 1)
